b4b127c50c2b06b86d30c5628bf2d3feb6c98eca
Alan Jui <alanjui.1960@gmail.com>
2015-03-02 22:53:06 +0800

    Connected Angular to Express

---
+++ client/app/scripts/controllers/signup.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:SignupCtrl
+ * @description
+ * # SignupCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('SignupCtrl', function ($scope, $http) {
+
+    var user, signup;
+    $scope.signup = signup = {};
+    signup.user = user = {};
+
+    signup.submit = function () {
+
+      // Make sure all fields are filled out
+      if (
+          !user.firstname ||
+          !user.lastname ||
+          !user.email ||
+          !user.password1 ||
+          !user.password2
+      ) {
+        alert('Please fill out all form fields.');
+        return false;
+      }
+
+      // Make sure the passwords match
+      if (user.password1 !== user.password2) {
+        alert('Your passwords must match.');
+        return false;
+      }
+
+      // Just for confirm that the bindings are working
+      console.log(user);
+
+      // Make the request to the server
+      var request = $http.post('/signup', user);
+
+      request.success(function (data) {
+        console.log(data.msg);
+      });
+
+      request.error(function (data) {
+        console.log(data.msg);
+      });
+    };
+
+  });
